e72af05fdaea3229425126767c1e5eddfcff98bc
Eric Dahl <eric.c.dahl@gmail.com>
2015-09-26 10:30:21 -0700

    Add RefreshScope

--- client/src/main/java/example/AppController.java
+++ client/src/main/java/example/AppController.java
@@ -1,10 +1,14 @@
 package example;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+// This will allow us to reinitialize this controller to get any new config
+// values when the /refresh endpoint is POSTed to.
+@RefreshScope
 public class AppController {
 
     @Value("${info.foo}")
